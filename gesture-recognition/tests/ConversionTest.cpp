164ed87687ea4a3a9fcd7ee80e138466106e8492
Quentin Dorleat <quentin.dorleat@gmail.com>
2012-11-28 19:15:53 +0100

    Conversion Test file. Bug on the repository

---
+++ gesture-recognition/tests/ConversionTest.cpp
@@ -0,0 +1,40 @@
+/* 
+ * File:   ConversionTest.cpp
+ * Author: Quentin
+ *
+ * Created on 26 nov. 2012, 19:11:29
+ */
+
+#include <stdlib.h>
+#include <iostream>
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/legacy/compat.hpp"
+
+#include "convert.hpp"
+#include "LoadYML.h"
+
+using namespace cv;
+using namespace pandore;
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    string ymlfile = ".\\img/1.yml";
+    string panfile = ".\\imgresult/1.pan";
+     
+    IplImage * imgyml = loadYml(ymlfile.c_str());
+    Mat img = IplToMat(*imgyml);
+    cvNamedWindow("test",1);
+    imshow("test", img);
+//    Img2duc imgpan = _MatToPan(img);
+//    imgpan.SaveFile(panfile.c_str());
+    
+//    system("C:/Program Files (x86)/pandore/bin/pvisu.exe "+"panfile");
+    
+    waitKey(0);
+    
+    return (EXIT_SUCCESS);
+}
