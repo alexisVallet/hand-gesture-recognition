49e04384ba73228b5f8ee7d7f35d6d4567970785
Quentin Dorleat <quentin.dorleat@gmail.com>
2012-11-28 19:16:31 +0100

    YML Loader written by Pierre

---
+++ gesture-recognition/LoadYML.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   LoadYML.h
+ * Author: Pierre
+ *
+ * Created on 26 novembre 2012, 19:07
+ */
+
+#ifndef LOADYML_H
+#define	LOADYML_H
+
+#include "opencv2/highgui/highgui.hpp"
+
+IplImage * loadYml(const char * filename);
+
+#endif	/* LOADYML_H */
+
