d327df8814bf0ecb80e035ead5e1378812b4de55
ALEXANDRE-BARFF <ALEXANDRE-BARFF@toros>
2012-11-30 20:45:05 -1000

    Adding the CPP File of the Convexity Classifier

---
+++ gesture-recognition/ConvexityClassifier.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   ConvexityClassifier.cpp
+ * Author: ALEXANDRE-BARFF
+ * 
+ * Created on 26 novembre 2012, 08:49
+ */
+
+#include "ConvexityClassifier.h"
+
+ConvexityClassifier::ConvexityClassifier() {
+}
+
+ConvexityClassifier::ConvexityClassifier(const ConvexityClassifier& orig) {
+}
+
+ConvexityClassifier::~ConvexityClassifier() {
+}
+
+int ConvexityClassifier::numberOfFingers(Mat &segmentedHand) {
+}
+
+int ConvexityClassifier::numberOfFingersFromClassifierResult(float classifierResult) {
+}
