2d5c4e0f6401a433e248a1b67d1dccd4f204ddfb
Welcome <torosdestroyer@gmail.com>
2012-12-26 10:22:30 -1000

    ConvexityClassifier_TEST.cpp Works!

--- gesture-recognition/tests/ConvexityClassifier_TEST.cpp
+++ gesture-recognition/tests/ConvexityClassifier_TEST.cpp
@@ -70,7 +70,7 @@ float Compute_Distance( Point a , Point b ){
     
     float d;
     
-    return d = sqrt( (float)pow((float) a.x - b.x , 2 ) + (float)pow((float) a.y - b.y , 2 ) );
+    return d = sqrt( (float)pow((float) a.x - b.x , 2 ) + pow((float) a.y - b.y , 2 ) );
     
 }
 
@@ -81,7 +81,6 @@ void ShowConvexityPoints(string filepath) {
     vector<Vec4i> hierarchy;
     vector<vector<Point> > contours_points;
     Mat segmentedHandRGB = imread(filepath);
-    imshow(filepath, segmentedHandRGB);
     
     /*CONVERSION FORMAT: Grayscale */
     vector<Mat> rgbPlanes;
@@ -89,24 +88,47 @@ void ShowConvexityPoints(string filepath) {
     Mat segmentedHandGray = rgbPlanes[0];
     
     /*Computing median filter*/
-    //medianBlur(segmentedHandGray,out,15);
+    medianBlur(segmentedHandGray,out,15);
     
     /*Looking for Contours Points*/
-    findContours( segmentedHandGray, contours_points, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE );
+    findContours( out, contours_points, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE );
     
-    /*Convert vector<vector<Point>> to vector<Point>*/
+    double Contour_tmp1, Contour_tmp2;
+    Contour_tmp1 = 0;
+    Contour_tmp2 = 0;
+    /*Convert vector<vector<Point>> to vector<Point> and find the biggest contours*/
+    vector<vector<Point> >::iterator it;
+    it = contours_points.begin();
+    while( it != contours_points.end() ){
+        
+        Contour_tmp1 = contourArea( *it , false );
+        
+        if( Contour_tmp1 > Contour_tmp2 ){
+            
+            Contour_tmp2 = Contour_tmp1;
+            contours = *it;
+            
+        }
+        
+        it++;
+        
+    }
+    
+    /* OLD VERSION
     for(int i = 0; i < contours_points.size() ; i++){
         for(int j = 0; j < contours_points[i].size() ; j++){
             contours.push_back(contours_points[i][j]);
         }
     }
+    /**/
     
     /*Drawing Contours*/
     Scalar color(rand()&255, rand()&255, rand()&255);
-    //drawContours(out, contours_points, 0, color, 1, 8, hierarchy);
+    drawContours(out, contours_points, 0, color, 1, 8, hierarchy);
     
     /*Approximation of Hands Contours by Polygon*/
-    approxPolyDP(contours,polygon,5,true);
+    approxPolyDP(contours,polygon,15,true);
+    contours = polygon;
     
     /*Drawing Identify Polygons*/
     //fillConvexPoly( out, polygon , color );
@@ -121,18 +143,20 @@ void ShowConvexityPoints(string filepath) {
     
     /*Convexity points Detection*/
     vector<vector<Point> > hull_points(contours_points.size());
-    vector<int> index_hull_points(contours_points.size());
-    vector<Point> convexityDefects(contours_points.size());
-    vector<Point> Concave_points;
-    vector<int> Convex_points;
     for(int i = 0 ; i< contours_points.size();i++)
     {
         convexHull(contours_points[i],hull_points[i],false,true); //Find the Convex of the hand
             
     }
     
+    
+    vector<int> index_hull_points(contours_points.size());
+    vector<Point> convexityDefects(contours_points.size());
+    vector<Point> Concave_points;
+    vector<int> Convex_points;
     convexHull(contours,index_hull_points,false,false); //Find the index of Convex points
-       
+    
+    /*Convexity Adapt from OpenCV [C versions]*/
     vector<Point>& contour = contours;
     vector<Point>& convexDefects = convexityDefects;
     vector<int>& hull = index_hull_points;
@@ -151,20 +175,20 @@ void ShowConvexityPoints(string filepath) {
     /*Filtering Concave points*/
     for(int i =0; i < convexDefects.size(); i++){
         /*Compare to the y-coordinate of the center of the palm*/
-        if( Palm.center.y > convexDefects[i].y )
+        if( Palm.center.y + 10 > convexDefects[i].y )
             Concave_points.push_back(convexDefects[i]);
     }
        
     /*Filtering Convex points*/
     for(int i =0; i < hull.size(); i++){
-        /*Compare to tje y-coordiante of the center of the palm*/
-        if( Palm.center.y > contour[hull[i]].y )
+        /*Compare to the y-coordiante of the center of the palm*/
+        if( Palm.center.y + 10 > contour[hull[i]].y )
             Convex_points.push_back(hull[i]);
     }
     
     cout << "First Filter Convex points: " << Convex_points.size() << endl;
     for (int i=0; i<Convex_points.size(); i++) {
-        circle(segmentedHandGray, contour[Convex_points[i]], 5, color, 1, 8, 0);
+        //circle(segmentedHandGray, contour[Convex_points[i]], 5, color, 1, 8, 0);
     }
        
     vector<int> tmp;
@@ -179,7 +203,7 @@ void ShowConvexityPoints(string filepath) {
             break;
         }
         
-        others : {
+        default : {
             
             float d;
             
@@ -188,18 +212,20 @@ void ShowConvexityPoints(string filepath) {
                 
                 /*Compute distance between each neighboors Convex_points*/
                 d = Compute_Distance( contour[Convex_points[i]] , contour[Convex_points[i+1]] );
+                cout << "Generics Distance " << d << endl;
                 
                 /*Minimum distance*/
-                if( d > 10 )
+                if( d > 20 )
                     tmp.push_back(Convex_points[i]);
                    
             }
             
             /*case: between the last point and the first point*/
-            d = Compute_Distance( contour[Convex_points[Convex_points.size()-1]] , contour[Convex_points[0]] );
+            d = Compute_Distance( contour[Convex_points[0]] , contour[Convex_points[Convex_points.size()-1]] );
+            cout << "Last Distance " << d << endl;
             
             /*Minimum distance*/
-            if( d > 10 )
+            if( d > 20 )
                 tmp.push_back(Convex_points[Convex_points.size()-1]);
             else if(Convex_points.size() == 2)
                 tmp.push_back(Convex_points[0]);
@@ -255,7 +281,7 @@ void ShowConvexityPoints(string filepath) {
     /*Drawing Convex of polygon*/
     for(int i = 0; i < contours_points.size() ; i++)
     {
-        drawContours( segmentedHandGray, contours_points, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+        //drawContours( segmentedHandGray, contours_points, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
         //drawContours( segmentedHandGray, hull_points, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
     }
     
@@ -267,7 +293,7 @@ void ShowConvexityPoints(string filepath) {
 
 int main(int argc, char** argv) {
     
-    ShowConvexityPoints("./runFolder/test-segmented-4.bmp");
+    ShowConvexityPoints("./runFolder/imgresult/cleaned.jpg");
     waitKey(0);
     
     return (EXIT_SUCCESS);
